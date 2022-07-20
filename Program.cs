60cb54b80ebe420eae520a00844881451f43c0d0
Christy Hartner <xtyvkv@gmail.com>
2022-07-20 17:48:17 -0400

    general progress / notes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace CoffeeShop
 {
