e343fa80a577000f3d8afdac0f8b36db51af5f0e
Christy Hartner <xtyvkv@gmail.com>
2022-07-25 20:12:59 -0400

    styling

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -18,16 +18,18 @@ namespace CoffeeShop.Controllers
             var productList = _CoffeeShopContext.Product.ToArray();
             return View(productList);
         }
-
-        public IActionResult Details(int Id, string Name, string Description, double Price, string Category)
+        public IActionResult Details(int Id)
         {
-            ViewData["Id"] = Id;
-            ViewData["Name"] = Name;
-            ViewData["Description"] = Description;
-            ViewData["Price"] = Price;
-            ViewData["Category"] = Category;  
-            return View();
-            // need to return view using Id
+            Product product = null;
+            foreach (var currProduct in _CoffeeShopContext.Product.ToArray())
+            {
+                if (currProduct.Id == Id)
+                {
+                    product = currProduct;
+                    break;
+                }
+            }
+            return View(product);
         }
     }
 }
